b437675552a0795ff586760c7269557be3e571c2
LeeHan <dlgks0733@gmail.com>
2022-04-20 14:27:16 +0900

    commit

---
+++ ToyProjectBack/src/main/java/com/hans/toyprojectback/ToyProjectBackApplication.java
@@ -0,0 +1,13 @@
+package com.hans.toyprojectback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyProjectBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyProjectBackApplication.class, args);
+	}
+
+}
---
+++ ToyProjectBack/src/test/java/com/hans/toyprojectback/ToyProjectBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hans.toyprojectback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyProjectBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
